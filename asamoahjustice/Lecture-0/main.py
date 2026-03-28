dd5983312997eb43d4f0ceaf4f912eb5ad9b6e44
unknown <apostlekwadwo719@gmail.com>
2026-03-28 13:04:09 +0000

    added a new branch

---
+++ asamoahjustice/Lecture-0/main.py
@@ -0,0 +1,18 @@
+# def user_name():
+#     name = input('type in your school name: ')
+#     print(f"Your school name is: {name}")
+
+# user_name()
+
+# def call_me(a):
+#     print('call me at 5')
+
+# call_me()
+# import dis
+def py(a, b, c):
+    c = 5
+    a = (b*b) + (c*c)
+    results = a
+    print(results)
+print(py(2, 4, 5))
+
